74a69d42046add4c33c204f9b31cc8cb7ca9d9a2
yasharcamp-coder <yasharcamp@gmail.com>
2026-09-23 12:02:45 +0300

    Initialize express app and serve index.html
    Fix app initialization and add route to serve index.html

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express=require('express');const path = require('path');
 const path=require('path');
 const Database=require('better-sqlite3');
-const app=express();
+const app=expressapp.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 const db=new Database('attendance.db');
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
